4852cafb65e00a910e3201ba918ae16c8d2f846e
ANATH <anathalie98@gmail.com>
2023-04-04 01:25:00 +0200

    html, css en proceso

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -13,6 +13,6 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/signup')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("signup.html")
