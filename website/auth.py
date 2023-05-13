ec8f33a57dd3864c84e920bd26ce5a09c7178fd5
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-05-13 17:41:56 +0200

    Añadido del modelo y funcionalidad de CRUD de los diagnosticos

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for, jsonify
 import re
 from website.logic import login_user, register_user, update_user, delete_user
+from website.models import User, Diagnostic
+from website.logic import create_diagnostic, read_all_diagnostics, read_diagnostic, proportionate_feedback, delete_diagnostic
 
 auth = Blueprint('auth', __name__)
 
@@ -13,14 +15,10 @@ def login():
 
         try:
             if login_user(username, password):
-                print("ha entrado")
                 return render_template("index.html", user=session.get('user_id'))
             else:
-                print("no ha entrado")
                 return redirect(url_for('auth.login'))
         except TypeError as e:
-            # Handle the TypeError here, for example, by logging the error
-            print("Error:", e)
             return redirect(url_for('auth.login'))
     else:
         return render_template("login.html", user=session.get('user_id'))
@@ -37,34 +35,25 @@ def signup():
         password = request.form.get('password')
         password2 = request.form.get('password2')
 
-        print(name, surname, surname2, username, email, password, password2)
 
         if password != password2:
-            print("erro1")
             return redirect(url_for('auth.signup'))
 
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            print("error2")
             return redirect(url_for('auth.signup'))
 
         if not re.match(r"[A-Za-z0-9]+", username):
-            print("error3")
             return redirect(url_for('auth.signup'))
         if not name or not surname or not username or not email or not password or not password2:
-            print("error4")
             return redirect(url_for('auth.signup'))
 
         if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])([a-zA-Z\d!@#$%^&*()]{8,})$", password):
-            print("valida los datos")
             return redirect(url_for('auth.signup'))
 
         register_user(name, surname, username, email, password, surname2)
-        print("usuario registrado")
         return redirect(url_for('auth.login'))
-        print("jajaj error1")
     else:
         return render_template("signup.html")
-    print("jajaj error2")
 
 
 @auth.route('/cuenta')
@@ -72,12 +61,10 @@ def cuenta():
     return render_template("cuenta.html")
 
 
-
 @auth.route('/editar', methods=['GET', 'POST'])
 def editar():
     if request.method == 'POST':
         if request.form['submit_button'] == 'btnUpdate':
-            print("Updating")
             name = request.form.get('name')
             surname = request.form.get('surname1')
             surname2 = request.form.get('surname2')
@@ -86,17 +73,73 @@ def editar():
             password = request.form.get('password')
             user_id = session.get('user_id')
             update_user(user_id, name, surname, username, email, password, surname2)
-            print("usuario actualizado")
         if request.form['submit_button'] == 'btnDelete':
-            print("Deleting")
             user_id = session.get('user_id')
             delete_user(user_id)
-            print("usuario eliminado")
         return redirect(url_for('auth.cuenta'))
     return render_template('editar.html')
 
 
-@auth.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('views.index'))
+@auth.route('/editar', methods=['GET', 'POST'])
+def editar():
+    if request.method == 'POST':
+        submit_button = request.form.get('submit_button')
+        user_id = session.get('user_id')
+
+        if submit_button == 'btnUpdate':
+            name = request.form.get('name')
+            surname = request.form.get('surname1')
+            surname2 = request.form.get('surname2')
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+
+            update_user(user_id, name, surname, username, email, password, surname2)
+
+        elif submit_button == 'btnDelete':
+            delete_user(user_id)
+
+        return redirect(url_for('auth.cuenta'))
+    return render_template('editar.html')
+
+
+@auth.route('/diagnostics', methods=['POST'])
+def diagnosticate():
+    user_id = session.get('user_id')
+    text = request.json.get('text')
+    create_diagnostic(user_id, text)
+    return jsonify({'status': 'success', 'message': 'Diagnostic created'}), 201
+
+
+@auth.route('/diagnostics', methods=['GET'])
+def read_all_diagnostics_route():
+    user_id = session.get('user_id')
+    diagnostics = read_all_diagnostics(user_id)
+    return jsonify(diagnostics), 200
+
+
+@auth.route('/diagnostic/<int:diagnostic_id>', methods=['GET'])
+def get_diagnostic(user_id: str, diagnostic_index: int) -> Diagnostic:
+    diagnostic = read_diagnostic(user_id, diagnostic_index)
+    return jsonify(diagnostic), 200
+
+@auth.route('/diagnostics/<int:diagnostic_id>', methods=['PUT'])
+def update_diagnostic_route(diagnostic_id):
+    user_id = session.get('user_id')
+    text = request.json.get('text')
+    correct_label = request.json.get('correct_label')
+    success = proportionate_feedback(user_id, diagnostic_id, text, correct_label)
+    if success:
+        return jsonify({'status': 'success', 'message': 'Diagnostic updated'}), 200
+    else:
+        return jsonify({'status': 'failure', 'message': 'Diagnostic not found'}), 404
+
+
+@auth.route('/diagnostics/<int:diagnostic_id>', methods=['DELETE'])
+def delete_diagnostic_route(diagnostic_id):
+    user_id = session.get('user_id')
+    success = delete_diagnostic(user_id, diagnostic_id)
+    if success:
+        return jsonify({'status': 'success', 'message': 'Diagnostic deleted'}), 200
+    else:
+        return jsonify({'status': 'failure', 'message': 'Diagnostic not found'}), 404
